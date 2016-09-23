394c140dd13e847b653d4055b4a8c56d7d370339
Georg Perhofer <georg.perhofer@aimsoftware.com>
2016-09-23 11:27:41 +0200

    Revert "working on: _setOptions"
    This reverts commit cd7a3199d0ee1fed8a8d78d948c437463521faad.

--- src/js/WinJS/Utilities/_Control.js
+++ src/js/WinJS/Utilities/_Control.js
@@ -40,13 +40,7 @@ define([
                     if ((ch1 === 'o' || ch1 === 'O') && (ch2 === 'n' || ch2 === 'N')) {
                         if (typeof value === "function") {
                             if (control.addEventListener) {
-                                var eventKey = key;
-                                // support mobile phones
-                                // http://stackoverflow.com/questions/2915833/how-to-check-browser-for-touchstart-support-using-js-jquery
-                                if(!'onclick' in control){
-                                    eventKey = "ontouchstart"
-                                }
-                                control.addEventListener(eventKey.substr(2), value);
+                                control.addEventListener(key.substr(2), value);
                                 continue;
                             }
                         }
